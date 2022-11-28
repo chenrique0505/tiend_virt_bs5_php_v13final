fbc2738a3b0ba9f448443d30da0325ea66bd8a1a
Carlos_Henriquez _0505 <79298840+chenrique0505@users.noreply.github.com>
2022-11-28 16:43:50 -0300

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 require 'dbconfig.php';
-$mysqli=new mysqli("sql300.epizy.com","epiz_33001403","GBhh5Hv69GlxgMy",'epiz_33001403_tienda_petscarlos');
+$mysqli=new mysqli("localhost","root","",$dbname);
 $link = mysqli_connect($dbhost, $dbusername,$dbpassword,$dbname);
 if(mysqli_connect_errno()){
     printf("conexion fallida: %s\n", mysqli_connect_error());/*el porcentaje mostrara el tipo error que devuelve mysqli_connect_error  */
